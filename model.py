58f4bec77bc24a9cce534a44aae986c7b378448d
Vatsal-Rajput <rajputchinu028@gmail.com>
2023-09-24 17:05:33 +0530

    first commit

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-
+#some changes
 # Load the training data
 train_data = pd.read_csv('/content/drive/MyDrive/dataset/train.csv')
 
